d6b4490d6e95417a0319c7c868d098eb45639145
Martijn Faber <martijn@fabtronics.nl>
2021-07-16 17:17:21 +0200

    feat(dashboard): add sidebar to dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,16 @@
 <template>
+  <SideBar />
   <h1>Hello World</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SideBar from "@/components/layout/SideBar.vue";
 
 export default defineComponent({
   name: "Dashboard",
+  components: {
+    SideBar,
+  },
 });
 </script>
