85f4957025c7f7f478d284e018864863604e219c
Martijn Faber <martijn@fabtronics.nl>
2021-07-16 17:13:36 +0200

    feat(icon): add icon component

---
+++ src/components/base/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <img :src="`/static/icons/${name}.svg`" class="icon" />
+</template>
+
+<style lang="scss" scoped>
+.icon {
+  width: 24px;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Icon",
+  props: {
+    name: {
+      type: String,
+    },
+  },
+});
+</script>
