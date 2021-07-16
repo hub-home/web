9e8521b3bc973b7987321308f7ed94461791015b
Martijn Faber <martijn@fabtronics.nl>
2021-07-16 17:14:44 +0200

    feat(column): add column component

---
+++ src/components/layout/Column.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="column">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.column {
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;
+}
+</style>
