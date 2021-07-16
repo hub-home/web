376ffce65710178701a2102b0609d7382ba69e2a
Martijn Faber <martijn@fabtronics.nl>
2021-07-16 17:17:46 +0200

    feat(listlayout): add listlayout component

---
+++ src/components/layout/ListLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="list">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.list {
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap);
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ListLayout",
+});
+</script>
